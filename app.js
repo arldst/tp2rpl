6a5f1d6e04cd9810ef30ee027986d3b783a3673e
arldst <ariel.jusuf@gmail.com>
2020-03-16 23:07:13 +0700

    registration page

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
-const DB_USER = [
-    {
-        username: "arldst",
-        password: "password"
-    }
-]
+let DB_USER = []
+
+console.log("Array DB_USER :");
+console.log(DB_USER);
 
 const inputUsername = document.querySelector("#username");
 const inputPassword = document.querySelector("#password");
+const inputKonfirmasiPassword = document.querySelector("#konfirmasiPassword")
 const buttonSubmit = document.querySelector("#submitForm");
 
 buttonSubmit.addEventListener('click', (e) => {
     e.preventDefault();
     const valueInputUsername = inputUsername.value;
     const valueInputPassword = inputPassword.value;
+    const valueInputKonfirmasiPassword = inputKonfirmasiPassword.value;
 
-    let flag = 0;
-    DB_USER.map((data) => {
-        if ((data.username === valueInputUsername) && (data.password === valueInputPassword)){
-            flag = 1;
+    const warningMsg1 = document.querySelector(".warning1");
+    const warningMsg2 = document.querySelector(".warning2");
+    const warningMsg3 = document.querySelector(".warning3");
+
+    if(valueInputUsername === "" || valueInputPassword === "" || valueInputKonfirmasiPassword === ""){
+        warningMsg1.className = "text-danger";
+    }
+
+    else {
+        const passwordString = valueInputPassword.localeCompare(valueInputKonfirmasiPassword);
+    
+        if(passwordString != 0){
+            warningMsg2.className = "text-danger";
         }
-    })
 
-    const warningMsg = document.querySelector(".warning");
+        else {
+            let check = false;
+            for(let i = 0; i < DB_USER.length; i++){
+                if(DB_USER[i].username === valueInputUsername){
+                    check = true;
+                    break;
+                }
+            }
 
-    if (flag === 0){
-        warningMsg.className = "text-danger";
-    } else {
-        alert("Selamat anda masuk ke home");
+            if(!check){
+                DB_USER.push({username:valueInputUsername, password:valueInputPassword });
+                console.log("Data telah dimasukan ke DB_USER :")
+                console.log(DB_USER);
+
+                alert("Selamat anda berhasil registrasi");
+            }
+
+            else {
+                warningMsg3.className = "text-danger";
+            }
+        }
     }
 })
 
